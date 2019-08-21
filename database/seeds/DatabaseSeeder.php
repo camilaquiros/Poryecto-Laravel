2056c13a5904c65624d1602922f7429e3cf9f7d1
silvizab <silviazabala1512@gmail.com>
2019-08-21 08:17:41 -0300

    arreglo database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -37,19 +37,17 @@ class DatabaseSeeder extends Seeder
         //factory(User::class)->times(10)->create();
         $avatars = factory(Avatar::class)->times(13)->create();
 
-<<<<<<< HEAD
         /*foreach ($products as $oneProduct) {
         $oneProduct->category()->associate($categories->random(1)->first()->id);
         $oneProduct->subcategory()->associate($subcategories->random(1)->first()->id);
         $oneProduct->save();
       };*/
-=======
+
       //   foreach ($products as $oneProduct) {
       //   $oneProduct->category()->associate($categories->random(1)->first()->id);
       //   $oneProduct->subcategory()->associate($subcategories->random(1)->first()->id);
       //   $oneProduct->save();
-      // };
->>>>>>> c9b1c36c8caeabda8347106e58f70033dd0265ca
+      // };c9b1c36c8caeabda8347106e58f70033dd0265ca
 
     }
 
