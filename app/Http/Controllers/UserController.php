09ec7401ddd2ee04f333c0f3f05bc045029c75ff
Camila <camiq_07@hotmail.com>
2019-08-27 02:44:41 -0300

    comento validacion de perfil porque no funciona

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,37 +19,37 @@ class UserController extends Controller
  public function update( Request $request)
  {
 
-   $rules =  [
-       'fullName' => 'required|string|max:255',
-       'username' => 'required|alpha_dash|max:20|min:5|unique:users,username',
-       'email' => 'required|email|unique:users,email',
-       'country' => 'required',
-       'shipping_address' => 'required',
-   ];
-
-   $messages =  [
-     'required' => 'Este campo es obligatorio',
-     'alpha' => 'Este campo debe contener solo letras',
-     'numeric'=> 'Este campo debe contener solo números',
-     'alpha_dash' => 'Este campo solo puede contener letras, numeros, guiones o guiones bajos',
-     'max' => 'Este campo puede tener como máximo :max caracteres',
-     'email' => 'Usá el formato nombre@ejemplo.com.',
-     'unique' => 'Este dato ya se encuentra registrado',
-     'confirmed' => 'Las contraseñas no coinciden',
-     'min' => 'Este campo debe tener como mínimo :min caracteres'
-   ];
-
-   $validator = \Validator::make(Input::all(),$rules,$messages);
-
-		if($validator->fails()){
-
-			return Redirect::to('/profile#edit')
-				->withErrors($validator)
-				->withInput(Input::except('password'));
-		} else{
+   // $rules =  [
+   //     'fullName' => 'required|string|max:255',
+   //     'username' => 'required|alpha_dash|max:20|min:5|unique:users,username',
+   //     'email' => 'required|email|unique:users,email',
+   //     'country' => 'required',
+   //     'shipping_address' => 'required',
+   // ];
+   //
+   // $messages =  [
+   //   'required' => 'Este campo es obligatorio',
+   //   'alpha' => 'Este campo debe contener solo letras',
+   //   'numeric'=> 'Este campo debe contener solo números',
+   //   'alpha_dash' => 'Este campo solo puede contener letras, numeros, guiones o guiones bajos',
+   //   'max' => 'Este campo puede tener como máximo :max caracteres',
+   //   'email' => 'Usá el formato nombre@ejemplo.com.',
+   //   'unique' => 'Este dato ya se encuentra registrado',
+   //   'confirmed' => 'Las contraseñas no coinciden',
+   //   'min' => 'Este campo debe tener como mínimo :min caracteres'
+   // ];
+   //
+   // $validator = \Validator::make(Input::all(),$rules,$messages);
+   //
+		// if($validator->fails()){
+   //
+		// 	return Redirect::to('/profile#edit')
+		// 		->withErrors($validator)
+		// 		->withInput(Input::except('password'));
+		// } else{
   $userToUpdate = User::find(auth()->user()->id)
   ->update($request->all());
   return redirect('/profile');
-}
+// }
 }
 }
